8022c911f42f13c3ec37658c7ab20c1d9f6dcec1
samer <samirmamer@gmail.com>
2023-08-18 21:34:15 -0400

    Configure slock

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "samer";
+static const char *group = "samer";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
